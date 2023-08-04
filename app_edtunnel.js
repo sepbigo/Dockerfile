b2821a70d33639a7357dd2820190d558eea5f96f
sepbigo <sep@aya.yale.edu>
2023-08-04 16:21:01 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'aa6d743f-9447-42cf-8485-dc39b97fffd9';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
